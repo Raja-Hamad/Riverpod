1a9fcbde7da8c612119cad2d445700533f7e6e30
Raja-Hamad <kayanihamad0316@gmail.com>
2026-09-25 14:14:33 +0500

    ADDED:Copwith method in product model class

--- lib/async_notifier_provider/crud_real_api/models/product.dart
+++ lib/async_notifier_provider/crud_real_api/models/product.dart
@@ -22,4 +22,19 @@ class Product {
       image: json['image'],
     );
   }
+    Product copyWith({
+    int? id,
+    String? title,
+    String? description,
+    double? price,
+    String? image,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      price: price ?? this.price,
+      image: image ?? this.image,
+    );
+  }
 }
